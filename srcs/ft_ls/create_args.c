b61605e270b8872d4038519076ba4dd59a65aaf0
Dav HOJT <davhojt@gmail.com>
2018-07-04 19:47:12 +0200

    Normed

--- srcs/ft_ls/create_args.c
+++ srcs/ft_ls/create_args.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/22 19:24:15 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/12 19:33:07 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/29 01:03:51 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_args				*create_args(void)
 {
 	t_args			*args;
 
-	if (!(args = (t_args *)malloc(sizeof(t_args))))//DONE
+	if (!(args = (t_args *)malloc(sizeof(t_args))))
 		return (NULL);
 	ft_bzero(args, sizeof(*args));
 	return (args);
--- srcs/issuance/path.c
+++ srcs/issuance/path.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/12 17:18:49 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/25 10:58:06 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/29 01:02:57 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void				path(t_frame *frame, t_args *args, char *path, char *name)
 	char			*new_path;
 	char			*tmp;
 
-	if (!(args->data.str = ft_strdup(name))) //DONE
+	if (!(args->data.str = ft_strdup(name)))
 		args->data.str = frame->malloc_failed;
-	if (!(new_path = ft_strnew(ft_strlen(path) + ft_strlen(name) + 1))) //DONE
+	if (!(new_path = ft_strnew(ft_strlen(path) + ft_strlen(name) + 1)))
 		args->data.path = frame->malloc_failed;
 	else
 	{
