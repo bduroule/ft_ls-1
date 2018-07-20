b74f5c0724b8e7ba685ee746abbaed4fb27d17a2
davhojt <davhojt@gmail.com>
2018-07-20 12:46:54 +0300

    Set out structure for issuance

--- srcs/issuance/issuance.c
+++ srcs/issuance/issuance.c
@@ -6,17 +6,26 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/20 11:55:05 by dhojt             #+#    #+#             */
-/*   Updated: 2018/07/20 12:25:06 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/07/20 12:46:23 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+
+static void			distribution(t_frame *frame)
+{
+	while (frame->args)
+	{
+		do_ls(frame);
+		frame->args = frame->args->next;
+	}
+}
 
 void				issuance(t_frame *frame)
 {
 	frame->sort = frame->args;
 	get_attributes(frame);
 	sort(frame);
-	if (frame)
-		;
+	frame->args = frame->sort;
+	distribution(frame);
 }
