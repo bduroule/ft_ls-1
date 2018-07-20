a8296d5942a546ebea9594f95c73767218e9ec84
davhojt <davhojt@gmail.com>
2018-07-20 14:22:50 +0300

    Figured how to access uid string.

--- srcs/issuance/get_attributes.c
+++ srcs/issuance/get_attributes.c
@@ -6,14 +6,26 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/20 12:25:28 by dhojt             #+#    #+#             */
-/*   Updated: 2018/07/20 12:25:56 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/07/20 14:21:21 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <uuid/uuid.h>
 
 void				get_attributes(t_frame *frame)
 {
-	if (frame)
-		;
+	t_args			*args;
+	struct stat		the_file;
+
+	args = frame->current_args;
+	while (args)
+	{
+		ft_printf("Result: %d\n", stat(args->str, &the_file));
+		ft_printf("ARG: %s (%s)\n", args->str, getpwuid(the_file.st_uid)->pw_name, );
+		args = args->next;
+	}
 }
