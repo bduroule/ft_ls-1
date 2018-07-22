c6acb4aaa9ca87b862c8999382d803112319853f
davhojt <davhojt@gmail.com>
2018-07-22 17:37:53 +0300

    issuance now calls loop functions.

--- srcs/display/no_file.c
+++ srcs/display/no_file.c
@@ -1,29 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   issuance.c                                         :+:      :+:    :+:   */
+/*   no_file.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/07/20 11:55:05 by dhojt             #+#    #+#             */
-/*   Updated: 2018/07/22 17:34:21 by dhojt            ###   ########.fr       */
+/*   Created: 2018/07/22 17:12:31 by dhojt             #+#    #+#             */
+/*   Updated: 2018/07/22 17:16:34 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-static void			distribution(t_frame *frame)
+void				no_file(t_frame *frame, t_args *args)
 {
-	loop_no_file(frame);
-	loop_files(frame);
-	loop_dirs(frame);
-}
+	t_data *data;
 
-void				issuance(t_frame *frame)
-{
-	frame->current_args = frame->args;
-	get_attributes(frame);
-	sort(frame);
-	frame->args = frame->current_args;
-	distribution(frame);
+	data = &args->data;
+	while (args && frame)
+	{
+		data = &args->data;
+		if (data->no_file)
+			ft_printf("ft_ls: %s: No such file or directory\n", data->str);
+		args = args->next;
+	}
 }
