8b18df3e4f45d3887f4d8c3b7461f848713dfd26
davhojt <davhojt@gmail.com>
2018-07-23 00:13:08 +0300

    Added function to capture path dynamically.

--- srcs/issuance/do_ls.c
+++ srcs/issuance/do_ls.c
@@ -6,12 +6,29 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/20 12:47:00 by dhojt             #+#    #+#             */
-/*   Updated: 2018/07/22 20:48:25 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/07/22 23:55:32 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 #include <sys/stat.h>
+
+static void			get_path(t_frame *frame, t_args *args, char *path, char *name)
+{
+	char			*new_path;
+	char			*tmp;
+
+	args->data.str = name;
+	if (!(new_path = ft_strnew(ft_strlen(path) + ft_strlen(name) + 1)))
+		error_exit(frame, "Malloc Failed [new_path]");
+	tmp = new_path;
+	while (path && *path)
+		*(tmp ++) = *(path ++);
+	*(tmp++) = '/';
+	while (name && *name)
+		*(tmp++) = *(name++);
+	args->data.path = new_path;
+}
 
 void				do_ls(t_frame *frame, t_args *args)
 {
@@ -22,7 +39,7 @@ void				do_ls(t_frame *frame, t_args *args)
 	struct dirent	*dir;
 
 	head = NULL;
-	if (!(directory = opendir(args->data.str)))
+	if (!(directory = opendir(args->data.path)))
 	{
 		ft_printf("ls: %s: Permission denied\n", args->data.str);
 		return ;
@@ -30,7 +47,9 @@ void				do_ls(t_frame *frame, t_args *args)
 	while ((dir = readdir(directory)))
 	{
 		tmp = create_args(frame);
+		get_path(frame, tmp, args->data.path, dir->d_name);
 		tmp->data.str = dir->d_name;
+		tmp->data.path = args->data.path;
 		if (!head)
 			head = tmp;
 		else
