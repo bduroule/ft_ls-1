59827418ca1501978fb1c84c6e8995b7eb6288e9
davhojt <davhojt@gmail.com>
2018-07-22 19:52:47 +0300

    do_ls now opens dir, reads files, creates list.

--- srcs/issuance/do_ls.c
+++ srcs/issuance/do_ls.c
@@ -6,15 +6,41 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/20 12:47:00 by dhojt             #+#    #+#             */
-/*   Updated: 2018/07/22 17:54:04 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/07/22 19:42:53 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 #include <sys/stat.h>
 
-void				do_ls(t_frame *frame)
+void				do_ls(t_frame *frame, t_args *args)
 {
-	if (frame)
-		is_file(frame->args);
+	t_args			*head;
+	t_args			*tmp;
+	t_args			*last_args;
+	DIR				*directory;
+	struct dirent	*dir;
+
+	head = NULL;
+	if (!(directory = opendir(args->data.str)))
+	{
+		ft_printf("ls: %s: Permission denied\n", args->data.str);
+		return ;
+	}
+	while ((dir = readdir(directory)))
+	{
+		tmp = create_args(frame);
+		tmp->data.str = dir->d_name;
+		if (!head)
+			head = tmp;
+		else
+			last_args->next = tmp;
+		last_args = tmp;
+	}
+	closedir(directory);
+	while (head)
+	{
+		ft_printf("%s\n", head->data.str);
+		head = head->next;
+	}
 }
