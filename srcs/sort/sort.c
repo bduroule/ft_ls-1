c23da1de3ebe54236370701fe25d065c4151ef67
davhojt <davhojt@gmail.com>
2018-07-22 12:02:48 +0300

    Modified to use frame for variables.

--- srcs/sort/sort.c
+++ srcs/sort/sort.c
@@ -6,39 +6,38 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/20 12:13:10 by dhojt             #+#    #+#             */
-/*   Updated: 2018/07/21 18:32:09 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/07/22 11:43:21 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-static int			swap(t_args *track) //ALPHABETICAL
+static int			swap(t_frame *frame) //ALPHABETICAL
 {
 	t_data			tmp;
-	if (ft_strcmp(track->data.str, track->next->data.str) > 0)
+	if (ft_strcmp(frame->track->data.str, frame->track->next->data.str) > 0)
 	{
-		tmp = track->data;
-		track->data = track->next->data;
-		track->next->data = tmp;
+		tmp = frame->track->data;
+		frame->track->data = frame->track->next->data;
+		frame->track->next->data = tmp;
 		return (1);
 	}
 	return (0);
 }
 
-static void			do_sort(t_args *head)
+static void			do_sort(t_frame *frame)
 {
 	int				sorted;
-	t_args			*track;
 
 	while (1)
 	{
-		track = head;
+		frame->track = frame->head;
 		sorted = 1;
-		while (track && track->next)
+		while (frame->track && frame->track->next)
 		{
-			if (swap(track))
+			if (swap(frame))
 				sorted = 0;
-			track = track->next;
+			frame->track = frame->track->next;
 		}
 		if (sorted)
 			break ;
@@ -47,10 +46,8 @@ static void			do_sort(t_args *head)
 
 void				sort(t_frame *frame)
 {
-	t_args			*head;
-
-	head = frame->current_args;
+	frame->head = frame->current_args;
 	// Chose sorting method
-	do_sort(head);
-	frame->current_args = head;
+	do_sort(frame);
+	frame->current_args = frame->head;
 }
