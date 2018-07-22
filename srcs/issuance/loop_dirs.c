fe83301a508ac5cb0f89a4fc8110f3b28b7341ee
davhojt <davhojt@gmail.com>
2018-07-22 19:52:19 +0300

    Issuance now routes through do_ls instead of print direct.

--- srcs/issuance/loop_dirs.c
+++ srcs/issuance/loop_dirs.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/22 17:32:15 by dhojt             #+#    #+#             */
-/*   Updated: 2018/07/22 17:33:41 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/07/22 17:55:32 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void				loop_dirs(t_frame *frame)
 	while (args)
 	{
 		if (!is_file(args) && !args->data.no_file)
-			display(frame, args);
+			do_ls(frame, args);
 		args = args->next;
 	}
 }
