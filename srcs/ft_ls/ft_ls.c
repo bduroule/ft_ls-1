bce31fee8867006c73b430dda347e37c20fd8c66
davhojt <davhojt@gmail.com>
2018-07-22 17:41:37 +0300

    Removed test print.

--- srcs/ft_ls/ft_ls.c
+++ srcs/ft_ls/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/13 01:28:20 by dhojt             #+#    #+#             */
-/*   Updated: 2018/07/21 18:28:58 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/07/22 17:41:09 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,6 @@ static void			ft_ls(t_frame *frame)
 {
 	parse(frame);
 	issuance(frame);
-
-	/*        TEST PRINT         */
-	t_args *args = frame->current_args;
-	t_data *data;
-	while (args)
-	{
-		data = &args->data;
-		if (data->no_file)
-			ft_printf("ft_ls: %s: No such file or directory\n", data->str);
-		args = args->next;
-	}
-	args = frame->current_args;
-	while (args)
-	{
-		data = &args->data;
-		if (!data->no_file)
-		ft_printf("%d  %-7o %s  %s %8d  %d  %-15.15s \n", data->ino, data->type, data->user, data->group, data->size, data->t_time, data->str);
-		args = args->next;
-	}
-	/*     END TEST PRINT        */
-
-
-
 	free_frame(frame);
 }
 
