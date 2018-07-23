69864b32825a819be3959a384f26180f5efc56f8
davhojt <davhojt@gmail.com>
2018-07-23 15:46:44 +0300

    Modified display to print permissions.

--- srcs/display/display.c
+++ srcs/display/display.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/22 16:35:50 by dhojt             #+#    #+#             */
-/*   Updated: 2018/07/23 09:43:35 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/07/23 15:41:46 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void				display(t_frame *frame, t_args *args)
 	t_data *data;
 
 	data = &args->data;
-	ft_printf("%lld  %-7o %s  %s %8d  %d  %-15.15s \n", data->ino, data->type, data->user, data->group, data->size, data->t_time, data->str);
+	ft_printf("%lld  -%s %s  %s %8d  %d  %-15.15s \n", data->ino, type(frame, args), data->user, data->group, data->size, data->t_time, data->str);
 	if (frame)
 		;
 }
