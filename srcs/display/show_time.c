09809d7cf69862077ef8ef4ad8f3b2aa3c8a1f65
davhojt <davhojt@gmail.com>
2018-08-05 15:59:15 +0300

    Added condition to print year when modification date condition is met.

--- srcs/display/show_time.c
+++ srcs/display/show_time.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/05 13:38:31 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/05 14:06:29 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/05 15:52:00 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@ void				show_time(t_frame *frame, t_args *args)
 	char			*time;
 
 	time = ctime(&args->data.t_time);
-	//ft_printf("%.24s ", ctime(&args->data.t_time));
 	ft_printf("%2.2s ", time + 8);
 	ft_printf("%3.3s ", time + 4);
-	ft_printf("%5.5s ", time + 11);
+	if (args->data.t_time < frame->time_6_months_ago || args->data.t_time > frame->time_now)
+		ft_printf("%5.4s ", time + 20);
+	else
+		ft_printf("%5.5s ", time + 11);
 	if (frame && args)
 		;
 }
