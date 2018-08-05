a06a916273accee257f3f36293ec18b29255858f
davhojt <davhojt@gmail.com>
2018-08-06 00:21:28 +0300

    Now get's user width

--- srcs/issuance/get_column_widths.c
+++ srcs/issuance/get_column_widths.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/05 23:43:22 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/06 00:13:40 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/06 00:20:43 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,15 @@ static void			calc_len_links(t_frame *frame, t_args *args)
 	}
 	if (len > frame->len_links)
 		frame->len_links = len;
+}
+
+static void			calc_len_user(t_frame *frame, t_args *args)
+{
+	int				len;
+
+	len = ft_strlen(args->data.user);
+	if (len > frame->len_user)
+		frame->len_user = len;
 }
 
 static void			calc_len_size(t_frame *frame, t_args *args)
@@ -56,6 +65,7 @@ void				get_column_widths(t_frame *frame, t_args *args)
 	while (head)
 	{
 		calc_len_links(frame, head);
+		calc_len_user(frame, head);
 		calc_len_size(frame, head);
 		head = head->next;
 	}
