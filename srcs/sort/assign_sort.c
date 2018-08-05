9eb86ce7229a8c7891b023b2ae548adc383d4d4a
davhojt <davhojt@gmail.com>
2018-08-05 20:10:04 +0300

    Made sort assination reverse friendly.

--- srcs/sort/assign_sort.c
+++ srcs/sort/assign_sort.c
@@ -6,16 +6,26 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/05 19:56:57 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/05 19:59:37 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/05 20:08:52 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
+static void			sort_reverse(t_frame *frame)
+{
+		frame->sort_function = &sort_alpha_r;
+}
+	
+static void			sort_normal(t_frame *frame)
+{
+		frame->sort_function = &sort_alpha;
+}
+	
 void				assign_sort(t_frame *frame)
 {
-	if (frame->option.r)
-		frame->sort_function = &sort_alpha_r;
+	if (!frame->option.r)
+		sort_normal(frame);
 	else
-		frame->sort_function = &sort_alpha;
+		sort_reverse(frame);
 }
