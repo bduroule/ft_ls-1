8a3a75bf965d274d6fbf135cdcdfaa21d5f186ab
davhojt <davhojt@gmail.com>
2018-08-05 21:24:29 +0300

    Reversed to match ls

--- srcs/sort/sort_time_t.c
+++ srcs/sort/sort_time_t.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/05 21:10:39 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/05 21:11:21 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/05 21:16:31 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 bool				sort_time_t(t_frame *frame)
 {
-	if (frame->track->data.t_time > frame->track->next->data.t_time)
+	if (frame->track->data.t_time < frame->track->next->data.t_time)
 		return (true);
 	return (false);
 }
