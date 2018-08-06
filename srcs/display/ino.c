db2767de1e4f9028353458c54f1d8e2de6a8726d
davhojt <davhojt@gmail.com>
2018-08-06 11:42:59 +0300

    Modified to correct type. for num in ino

--- srcs/display/ino.c
+++ srcs/display/ino.c
@@ -5,15 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/07/23 15:53:30 by dhojt             #+#    #+#             */
-/*   Updated: 2018/07/23 15:57:52 by dhojt            ###   ########.fr       */
+/*   Created: 2018/08/06 01:40:13 by dhojt             #+#    #+#             */
+/*   Updated: 2018/08/06 11:41:28 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
+static int			get_diff(t_frame *frame, t_args *args)
+{
+	int				len;
+	long long		num;
+
+	len = 1;
+	num = args->data.ino;
+	while (num >= 10)
+	{
+		num /= 10;
+		len++;
+	}
+	return (frame->len_ino - len);
+}
+
 void				ino(t_frame *frame, t_args *args)
 {
-	if (frame->option.i)
-		ft_printf("%lld ", args->data.ino);
+	if (!frame->option.i)
+		return ;
+	print_spaces(get_diff(frame, args));
+	ft_printf("%lld ", args->data.ino);
 }
