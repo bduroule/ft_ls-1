33cf33ebef612252826b1734ee8c713c6c477823
davhojt <davhojt@gmail.com>
2018-08-06 12:59:03 +0300

    created reverse for time.

--- srcs/sort/sort_time_t_r.c
+++ srcs/sort/sort_time_t_r.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/08/05 23:14:27 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/05 23:14:46 by dhojt            ###   ########.fr       */
+/*   Created: 2018/08/05 21:10:39 by dhojt             #+#    #+#             */
+/*   Updated: 2018/08/06 12:47:04 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,28 @@
 
 bool				sort_time_t_r(t_frame *frame)
 {
-	if (frame->track->data.t_time > frame->track->next->data.t_time)
-		return (true);
-	return (false);
+	if (frame->option.c)
+	{
+		if (frame->track->data.c_time > frame->track->next->data.c_time)
+			return (true);
+		return (false);
+	}
+	if (frame->option.u)
+	{
+		if (frame->track->data.u_time > frame->track->next->data.u_time)
+			return (true);
+		return (false);
+	}
+	if (frame->option.U)
+	{
+		if (frame->track->data.U_time > frame->track->next->data.U_time)
+			return (true);
+		return (false);
+	}
+	else
+	{
+		if (frame->track->data.t_time > frame->track->next->data.t_time)
+			return (true);
+		return (false);
+	}
 }
