e3d5e18a6b6e2d7295390339b0192842f369fb4c
Dav HOJT <davhojt@gmail.com>
2018-08-11 22:05:11 +0200

    Seperated free function out of free_args_loop

--- srcs/ft_ls/free_frame.c
+++ srcs/ft_ls/free_frame.c
@@ -6,27 +6,32 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/13 22:44:38 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/11 21:22:40 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/11 22:04:10 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
+static void		free_individual_arg(t_args *args)
+{
+	free(args->data.str);
+	free(args->data.path);
+	free(args->data.user);
+	free(args->data.group);
+}
+
 void			free_args(t_args **args)//Break out - should not be static
 {
 	while (*args)
 	{
-		free ((*args)->data.str);
-		free ((*args)->data.path);
-		free ((*args)->data.user);
-		free ((*args)->data.group);
+		free_individual_arg(*args);
 		*args = (*args)->next;
 	}
 }
 
 void				free_frame(t_frame *frame)
 {
-	//free_args(&frame->args);
+	free_args(&frame->args);
 	//free everything LOL.
 	if (frame)
 		;
