4843d539632207cb9a5a615ab4993e1e6bb86c7e
davhojt <davhojt@gmail.com>
2018-08-11 21:53:18 +0200

    Added free_frame (progress from MacBook).

--- srcs/ft_ls/free_frame.c
+++ srcs/ft_ls/free_frame.c
@@ -6,14 +6,27 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/13 22:44:38 by dhojt             #+#    #+#             */
-/*   Updated: 2018/07/14 00:42:00 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/11 21:22:40 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
+void			free_args(t_args **args)//Break out - should not be static
+{
+	while (*args)
+	{
+		free ((*args)->data.str);
+		free ((*args)->data.path);
+		free ((*args)->data.user);
+		free ((*args)->data.group);
+		*args = (*args)->next;
+	}
+}
+
 void				free_frame(t_frame *frame)
 {
+	//free_args(&frame->args);
 	//free everything LOL.
 	if (frame)
 		;
