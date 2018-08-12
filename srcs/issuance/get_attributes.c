76c11f854709575afe7361364ede9a6a2bc36b21
Dav HOJT <davhojt@gmail.com>
2018-08-12 13:21:48 +0200

    Strings are now malloced.

--- srcs/issuance/get_attributes.c
+++ srcs/issuance/get_attributes.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/20 12:25:28 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/07 11:54:38 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/12 13:21:32 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,8 +53,8 @@ void				get_attributes(t_frame *frame)
 			args->data.ino = the_file.st_ino;
 			args->data.type = the_file.st_mode;
 			args->data.links = the_file.st_nlink;
-			args->data.user = getpwuid(the_file.st_uid)->pw_name;
-			args->data.group = getgrgid(the_file.st_gid)->gr_name;
+			args->data.user = ft_strdup(getpwuid(the_file.st_uid)->pw_name);//Careful
+			args->data.group = ft_strdup(getgrgid(the_file.st_gid)->gr_name);//Careful
 			args->data.size = the_file.st_size;
 			args->data.c_time = the_file.st_ctimespec.tv_sec;
 			args->data.t_time = the_file.st_mtimespec.tv_sec;
