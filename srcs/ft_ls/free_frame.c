2cef0d2871178c18697732f196727fc3aa3c40fa
Dav HOJT <davhojt@gmail.com>
2018-08-12 18:05:16 +0200

    Now free argv_options in frame.

--- srcs/ft_ls/free_frame.c
+++ srcs/ft_ls/free_frame.c
@@ -6,32 +6,16 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/13 22:44:38 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/11 22:04:10 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/12 17:57:55 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-static void		free_individual_arg(t_args *args)
-{
-	free(args->data.str);
-	free(args->data.path);
-	free(args->data.user);
-	free(args->data.group);
-}
-
-void			free_args(t_args **args)//Break out - should not be static
-{
-	while (*args)
-	{
-		free_individual_arg(*args);
-		*args = (*args)->next;
-	}
-}
-
 void				free_frame(t_frame *frame)
 {
 	free_args(&frame->args);
+	free(frame->argv_options);
 	//free everything LOL.
 	if (frame)
 		;
--- srcs/issuance/do_ls.c
+++ srcs/issuance/do_ls.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/20 12:47:00 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/12 17:32:08 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/12 17:33:34 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,5 +72,4 @@ void				do_ls(t_frame *frame, t_args *args)
 		}
 	}
 	free_args(&head);
-	//free head
 }
