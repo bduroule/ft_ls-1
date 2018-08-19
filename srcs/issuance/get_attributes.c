0f283fa8d14af3435dcd74dc6a76ae0af0034b44
davhojt <davhojt@gmail.com>
2018-08-19 22:10:54 +0200

    Fixed invalid read and segfault if user or group returns NULL.

--- srcs/issuance/get_attributes.c
+++ srcs/issuance/get_attributes.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/20 12:25:28 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/17 16:40:50 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/19 22:10:14 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,8 @@ void				get_attributes(t_frame *frame)
 {
 	t_args			*args;
 	struct stat		the_file;
+	struct passwd	*passwd;
+	struct group	*group;
 
 	args = frame->current_args;
 	while (args)
@@ -66,8 +68,14 @@ void				get_attributes(t_frame *frame)
 			args->data.ino = the_file.st_ino;
 			args->data.type = the_file.st_mode;
 			args->data.links = the_file.st_nlink;
-			args->data.user = ft_strdup(getpwuid(the_file.st_uid)->pw_name);//Careful
-			args->data.group = ft_strdup(getgrgid(the_file.st_gid)->gr_name);//Careful
+			if ((passwd = getpwuid(the_file.st_uid)))
+				args->data.user = ft_strdup(passwd->pw_name);//Careful
+			else
+				args->data.user = ft_strdup(ft_itoa(the_file.st_uid));//Careful
+			if ((group = getgrgid(the_file.st_gid)))
+				args->data.group = ft_strdup(group->gr_name);//Careful
+			else
+				args->data.group = ft_strdup(ft_itoa(the_file.st_gid));//Careful
 			args->data.size = the_file.st_size;
 			args->data.c_time = the_file.st_ctimespec.tv_sec;
 			args->data.t_time = the_file.st_mtimespec.tv_sec;
