08bba143d78b8ba70a8727df2ac9724c1adcbfaf
davhojt <davhojt@gmail.com>
2018-08-23 11:42:32 +0200

    Check if headers should be displayed Checks if more than one argument at the command line is a directory

--- srcs/issuance/issuance.c
+++ srcs/issuance/issuance.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/20 11:55:05 by dhojt             #+#    #+#             */
-/*   Updated: 2018/07/22 17:34:21 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/23 11:41:46 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,25 @@ static void			distribution(t_frame *frame)
 	loop_dirs(frame);
 }
 
+static void			get_headers(t_frame *frame)
+{
+	t_args			*args;
+
+	args = frame->args;
+	while (args && !frame->headers)
+	{
+		if (args->data.dir && !frame->option.d)
+			frame->headers = 1;
+		args = args->next;
+	}
+}
+
 void				issuance(t_frame *frame)
 {
 	frame->current_args = frame->args;
 	get_attributes(frame);
 	sort(frame);
 	frame->args = frame->current_args;
+	get_headers(frame);
 	distribution(frame);
 }
