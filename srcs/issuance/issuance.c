dd975d80c2679d48b7b8752babfb5eb20302487d
davhojt <davhojt@gmail.com>
2018-08-23 12:19:16 +0200

    Headers now compliants with regular files.

--- srcs/issuance/issuance.c
+++ srcs/issuance/issuance.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/20 11:55:05 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/23 11:41:46 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/23 12:17:48 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,22 @@ static void			distribution(t_frame *frame)
 	loop_dirs(frame);
 }
 
-static void			get_headers(t_frame *frame)
+static void			check_headers(t_frame *frame)
 {
 	t_args			*args;
+	int				headers;
+	int				not_dir;
 
 	args = frame->args;
+	headers = 0;
+	not_dir = 0;
 	while (args && !frame->headers)
 	{
+		if (!not_dir && !args->data.dir && !frame->option.d)
+			not_dir++;
 		if (args->data.dir && !frame->option.d)
+			headers++;
+		if (headers > 1 || (headers && not_dir))
 			frame->headers = 1;
 		args = args->next;
 	}
@@ -38,6 +46,6 @@ void				issuance(t_frame *frame)
 	get_attributes(frame);
 	sort(frame);
 	frame->args = frame->current_args;
-	get_headers(frame);
+	check_headers(frame);
 	distribution(frame);
 }
