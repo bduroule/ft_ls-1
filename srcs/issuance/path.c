7348862376e60c27fc51483a8a04c9f22d289c66
davhojt <davhojt@gmail.com>
2018-08-25 10:58:49 +0200

    Protected malloc for str and path.

--- srcs/issuance/path.c
+++ srcs/issuance/path.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/12 17:18:49 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/15 17:52:36 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/25 10:58:06 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,21 @@ void				path(t_frame *frame, t_args *args, char *path, char *name)
 	char			*new_path;
 	char			*tmp;
 
-	if (!(args->data.str = ft_strdup(name)))
-		error_exit(frame, "Malloc Failed [new_name]");// Handle carefully (recurse)
-	if (!(new_path = ft_strnew(ft_strlen(path) + ft_strlen(name) + 1)))
-		error_exit(frame, "Malloc Failed [new_path]");// Handle carefully (recurse)
-	tmp = new_path;
-	if (ft_strcmp(path, "."))
+	if (!(args->data.str = ft_strdup(name))) //DONE
+		args->data.str = frame->malloc_failed;
+	if (!(new_path = ft_strnew(ft_strlen(path) + ft_strlen(name) + 1))) //DONE
+		args->data.path = frame->malloc_failed;
+	else
 	{
-		while (path && *path)
-			*(tmp++) = *(path++);
-		*(tmp++) = '/';
+		tmp = new_path;
+		if (ft_strcmp(path, "."))
+		{
+			while (path && *path)
+				*(tmp++) = *(path++);
+			*(tmp++) = '/';
+		}
+		while (name && *name)
+			*(tmp++) = *(name++);
+		args->data.path = new_path;
 	}
-	while (name && *name)
-		*(tmp++) = *(name++);
-	args->data.path = new_path;
 }
