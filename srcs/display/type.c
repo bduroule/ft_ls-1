d6493831165d59343c44e0386daacaec5772b9fc
davhojt <davhojt@gmail.com>
2018-08-26 16:22:39 +0200

    Now prints correct third character SsTtx-

--- srcs/display/type.c
+++ srcs/display/type.c
@@ -6,12 +6,13 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/23 13:42:45 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/26 16:02:48 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/26 16:21:09 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 #include <sys/acl.h>
+#include <sys/stat.h>
 #include <sys/xattr.h>
 
 static char			*get_str(int value)
@@ -64,6 +65,16 @@ static char			get_acl(t_args *args)
 	return (' ');
 }
 
+static void			swap_exec_char(t_args *args, int i, char *exec_char)
+{
+	if (i == 2 && args->data.mode & S_ISUID)
+		*exec_char = ((args->data.mode & S_IXUSR) ? 's' : 'S');
+	if (i == 1 && args->data.mode & S_ISGID)
+		*exec_char = ((args->data.mode & S_IXGRP) ? 's' : 'S');
+	if (i == 0 && args->data.mode & S_ISVTX)
+		*exec_char = ((args->data.mode & S_IXOTH) ? 't' : 'T');
+}
+
 void				type(t_frame *frame, t_args *args)
 {
 	int				i;
@@ -80,6 +91,7 @@ void				type(t_frame *frame, t_args *args)
 		while (i--)
 		{
 			ft_strcpy(tmp, get_str((type >> (3 * i)) & 7));
+			swap_exec_char(args, i, tmp + 2);
 			tmp += 3;
 		}
 		*(tmp++) = get_acl(args);
