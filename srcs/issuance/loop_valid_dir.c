087eb6c16e4de5bba8b19019aa48ad70d96b51ee
Dav HOJT <davhojt@gmail.com>
2018-08-27 20:19:27 +0200

    normed

--- srcs/issuance/loop_valid_dir.c
+++ srcs/issuance/loop_valid_dir.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/22 20:03:57 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/23 18:16:04 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/27 20:18:17 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,27 @@ static void			get_column_widths(t_frame *frame, t_args *args)
 	}
 }
 
+static void			loop_args(t_frame *frame, t_args *args,
+		int *position_on_row)
+{
+	while (args)
+	{
+		if (!args->data.no_file)
+		{
+			display(frame, args);
+			*position_on_row += 1;
+			if (*position_on_row >= frame->number_of_columns)
+			{
+				ft_putchar('\n');
+				*position_on_row = 0;
+			}
+			else if (!frame->option.x)
+				ft_putchar('\n');
+		}
+		args = args->next;
+	}
+}
+
 void				loop_valid_dir(t_frame *frame, t_args *head)
 {
 	t_args			*args;
@@ -59,22 +80,7 @@ void				loop_valid_dir(t_frame *frame, t_args *head)
 		ft_printf("total %d\n", frame->total_blocks);
 	get_column_widths(frame, head);
 	calculate_number_of_columns(frame);
-	while (args)
-	{
-		if (!args->data.no_file)
-		{
-			display(frame, args);
-			position_on_row++;
-			if (position_on_row >= frame->number_of_columns)
-			{
-				ft_putchar('\n');
-				position_on_row = 0;
-			}
-			else if (!frame->option.x)
-				ft_putchar('\n');
-		}
-		args = args->next;
-	}
+	loop_args(frame, args, &position_on_row);
 	if (position_on_row && frame->option.x)
 		ft_putchar('\n');
 	if (frame->option.N)
