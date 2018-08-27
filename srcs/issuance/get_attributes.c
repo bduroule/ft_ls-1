4d183bf4dc59d42419a34f8b2403d379aaf033af
Dav HOJT <davhojt@gmail.com>
2018-08-27 20:19:43 +0200

    Normed.

--- srcs/issuance/get_attributes.c
+++ srcs/issuance/get_attributes.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/20 12:25:28 by dhojt             #+#    #+#             */
-/*   Updated: 2018/08/26 17:22:42 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/08/27 20:13:51 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,49 +53,53 @@ static void			get_type(t_args *args)
 		args->data.blk = 1;
 }
 
-static void			get_user_group(t_frame *frame, t_args *args, struct stat *the_file)
+static void			get_user_group(t_frame *frame, t_args *args, struct stat *f)
 {
 	struct passwd	*passwd;
 	struct group	*group;
-	if (frame)
-		;
 
-	if ((passwd = getpwuid(the_file->st_uid)))
-		(args->data.user = ft_strdup(passwd->pw_name));//Careful
+	if ((passwd = getpwuid(f->st_uid)))
+		(args->data.user = ft_strdup(passwd->pw_name));//DONE
 	else
-		args->data.user = ft_itoa(the_file->st_uid);//Careful
+		args->data.user = ft_itoa(f->st_uid);//DONE
 	if (!args->data.user)
 		args->data.user = frame->malloc_failed;
-	if ((group = getgrgid(the_file->st_gid)))
-		args->data.group = ft_strdup(group->gr_name);//Careful
+	if ((group = getgrgid(f->st_gid)))
+		args->data.group = ft_strdup(group->gr_name);//DONE
 	else
-		args->data.group = ft_itoa(the_file->st_gid);//Careful
+		args->data.group = ft_itoa(f->st_gid);//DONE
+	if (!args->data.group)
+		args->data.group = frame->malloc_failed;
 }
+
+void				read_f(t_frame *frame, t_args *args, struct stat *f)
+{
+	frame->total_blocks += f->st_blocks;
+	args->data.ino = f->st_ino;
+	args->data.mode = f->st_mode;
+	args->data.type = f->st_mode;
+	args->data.links = f->st_nlink;
+	get_user_group(frame, args, f);
+	args->data.rdev = f->st_rdev;
+	args->data.size = f->st_size;
+	args->data.c_time = f->st_ctimespec.tv_sec;
+	args->data.t_time = f->st_mtimespec.tv_sec;
+	args->data.u_time = f->st_atimespec.tv_sec;
+	args->data.U_time = f->st_birthtimespec.tv_sec;
+	get_type(args);
+}
+
 
 void				get_attributes(t_frame *frame)
 {
 	t_args			*args;
-	struct stat		the_file;
+	struct stat		f;
 
 	args = frame->current_args;
 	while (args)
 	{
-		if (!lstat(args->data.path, &the_file)) //check for sym links
-		{
-			frame->total_blocks += the_file.st_blocks;
-			args->data.ino = the_file.st_ino;
-			args->data.mode = the_file.st_mode;
-			args->data.type = the_file.st_mode;
-			args->data.links = the_file.st_nlink;
-			get_user_group(frame, args, &the_file);
-			args->data.rdev = the_file.st_rdev;
-			args->data.size = the_file.st_size;
-			args->data.c_time = the_file.st_ctimespec.tv_sec;
-			args->data.t_time = the_file.st_mtimespec.tv_sec;
-			args->data.u_time = the_file.st_atimespec.tv_sec;
-			args->data.U_time = the_file.st_birthtimespec.tv_sec;
-			get_type(args);
-		}
+		if (!lstat(args->data.path, &f))
+			read_f(frame, args, &f);
 		else
 			args->data.no_file = 1;
 		args = args->next;
